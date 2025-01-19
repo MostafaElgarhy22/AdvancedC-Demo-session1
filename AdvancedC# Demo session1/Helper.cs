665dc79b8e767b6908771b599d07681c35a9c100
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-19 22:10:57 +0200

    Add project files.

---
+++ AdvancedC# Demo session1/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedC__Demo_session1
+{
+    internal static class Helper
+    {
+
+
+        public static void Swap(ref int x, ref int y)
+        {
+            Console.WriteLine("========Swap========");
+
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+
+    }
+}
---
+++ AdvancedC# Demo session1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AdvancedC__Demo_session1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 3;
+            int B = 4;
+            Console.WriteLine($"A = {A}");
+            Console.WriteLine($"B = {B}");
+            Helper.Swap( ref A, ref B );
+            Console.WriteLine($"A = {A}");
+            Console.WriteLine($"B = {B}");
+        }
+    }
+}
