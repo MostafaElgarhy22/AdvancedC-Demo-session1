da85bded5ee2fcb8111a198325b29b3dd3c0fabb
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-19 22:33:11 +0200

    Swap Example [Video 1]

--- AdvancedC# Demo session1/Helper.cs
+++ AdvancedC# Demo session1/Helper.cs
@@ -8,17 +8,42 @@ namespace AdvancedC__Demo_session1
 {
     internal static class Helper
     {
+        //public static void Swap(ref object x, ref object y)
+        //{
+        //    Console.WriteLine("========Swap========");
+
+        //    object temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+
+        //public static void Swap(ref int x, ref int y)
+        //{
+        //    Console.WriteLine("========Swap========");
+
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+
+        //public static void Swap(ref Point x, ref Point y)
+        //{
+        //    Console.WriteLine("========Swap========");
+
+        //    Point temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
 
 
-        public static void Swap(ref int x, ref int y)
+        public static void Swap<T>(ref T x, ref T y)
         {
             Console.WriteLine("========Swap========");
 
-            int temp = x;
+           T temp = x;
             x = y;
             y = temp;
         }
-
 
     }
 }
--- AdvancedC# Demo session1/Program.cs
+++ AdvancedC# Demo session1/Program.cs
@@ -4,13 +4,20 @@
     {
         static void Main(string[] args)
         {
-            int A = 3;
-            int B = 4;
-            Console.WriteLine($"A = {A}");
-            Console.WriteLine($"B = {B}");
-            Helper.Swap( ref A, ref B );
-            Console.WriteLine($"A = {A}");
-            Console.WriteLine($"B = {B}");
+            #region Swap Example [Video 1]
+            //int A = 3;    
+            //int B = 4;
+            //Console.WriteLine($"A = {A}");
+            //Console.WriteLine($"B = {B}");
+            //Helper.Swap/*<int>*/(ref A, ref B);
+            //Console.WriteLine($"A = {A}");
+            //Console.WriteLine($"B = {B}");
+
+
+            //object Point p01 = new Point(10, 20);  // Boxing
+            //object Point p02 = new Point(50, 60);
+            // Helper.Swap(ref p01, ref p02);
+            #endregion
         }
     }
 }
